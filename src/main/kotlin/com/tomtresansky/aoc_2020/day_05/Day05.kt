252efed232bad34355701f88842fe2fd028e982f
Tom Tresansky <tom.tresansky@gmail.com>
2020-12-11 11:25:00 -0500

    Init Day 5.

---
+++ src/main/kotlin/com/tomtresansky/aoc_2020/day_05/Day05.kt
@@ -0,0 +1,16 @@
+package com.tomtresansky.aoc_2020.day_05
+
+@Suppress("SameParameterValue")
+class Day05 {
+    companion object {
+        const val INPUT_FILE_NAME = "day_05_input.txt"
+    }
+
+    fun solvePart1(): Int {
+        return 1
+    }
+
+    fun solvePart2(): Int {
+        return 2
+    }
+}
---
+++ src/test/kotlin/com/tomtresansky/aoc_2020/day_05/Day05Test.kt
@@ -0,0 +1,19 @@
+package com.tomtresansky.aoc_2020.day_05
+
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+    @Test
+    fun solvePart1() {
+        val problem = Day05()
+        val solution = problem.solvePart1()
+        println ("Day 5, Part 1: $solution")
+    }
+
+    @Test
+    fun solvePart2() {
+        val problem = Day05()
+        val solution = problem.solvePart2()
+        println ("Day 5, Part 2: $solution")
+    }
+}
